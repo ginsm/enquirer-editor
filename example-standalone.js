e084b27ba226374ff6121fa60788a760b49341a0
ginsm <matt@mgin.me>
2019-12-30 00:42:43 -0500

    Update readability

--- example-standalone.js
+++ example-standalone.js
@@ -4,11 +4,11 @@ const EditorPrompt = require('./index.js');
 // Create a new instance of the prompt
 const prompt = new EditorPrompt({
   type: 'editor',
-  name: 'editor',
   message: 'Opens an editor to edit a string',
   initial: 'String to be edited',
 });
 
 // Run the prompt
 prompt.run()
-    .then(console.log).catch(console.error);
+    .then(console.log)
+    .catch(console.error);
